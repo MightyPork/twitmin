740124cde25730bc48076e3df90705c2c65d68d5
Ondřej Hruška <ondra@ondrovo.com>
2016-04-13 14:47:30 +0200

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var elixir = require('laravel-elixir');
+var autoprefixer = require('autoprefixer');
+
+elixir.config.css.autoprefix.options.browsers = ["> 2%"];
+elixir.config.css.autoprefix.options.flexbox = "no-2009";
+elixir.config.css.cssnano.pluginOptions.discardComments = {removeAll: true};
+elixir.config.js.uglify.options.compress.drop_console = false;
+
+elixir.config.assetsPath='.';
+elixir.config.publicPath='.';
+
+elixir(function (mix) {
+	// Compile CSS
+	mix.sass('sass/app.scss', 'css/app.css');
+
+	// JS libs
+	mix.scripts([
+		'js-src/lib/lodash.custom.js',
+		'js-src/lib/jquery.js'
+	], 'js/libs.js');
+
+	mix.scripts([
+		'js-src/app.js'
+	], 'js/app.js');
+});
---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Tweet Compressor</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<div class="Container">
+	<h1>Tweet Compressor</h1>
+	<form method="POST">
+		<label for="tweet">Enter Your Tweet:</label>
+		<textarea name="tweet" id="tweet"></textarea>
+	</form>
+
+	<div id="resultbox"></div>
+	<textarea name="result" id="result"></textarea>
+</div>
+</body>
+</html>
