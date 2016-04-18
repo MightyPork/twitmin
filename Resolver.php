eef3bffb758b590674ea9a951cf6fea8d148bdcc
Ondřej Hruška <ondra@ondrovo.com>
2016-04-18 20:36:14 +0200

    comments

--- Resolver.php
+++ Resolver.php
@@ -120,7 +120,7 @@ class Resolver
 		$tweet = str_replace('...', '…', $tweet);
 
 		// *** Safeguards ***
-		// Blame @mvilcis. Thanks @freundTech for debugging.
+		// Blame @mvilcis. Thanks @freundTech for testing.
 		$tweet = preg_replace_callback('/((?:gnu\/|arch|\b)linux)(\s+)(is\s+bad|sucks(\s+dick|\s+balls|))\b/im', function($m) {
 			$linux = $m[1];
 			if (strtolower($linux) == 'linux') {
